a554113ef31eb712474d7c528ab1ff6998062973
Robel <manoarobel@gmail.com>
2025-11-26 09:40:50 +0400

    Fix: RSA key size upgraded to 2048 bits to satisfy SonarCloud

--- security/rsa_utils.py
+++ security/rsa_utils.py
@@ -4,7 +4,7 @@ from cryptography.hazmat.primitives import serialization
 def generate_rsa_keypair():
     private_key = rsa.generate_private_key(
         public_exponent=65537,
-        key_size=1024
+        key_size=2048
     )
     public_key = private_key.public_key()
 
